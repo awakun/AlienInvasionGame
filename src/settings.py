1649383147edfeb1e7cc113f0ab4af0fa08143e7
Dave Kaylor <worgenftw@gmail.com>
2021-03-23 21:02:49 -0700

    Adding settings module

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """The settings for the AlienInvasion game."""
+
+    def __init__(self):
+        """Initialize the settings."""
+        # Screen settings
+        self.screen_width = 2560
+        self.screen_height = 1440
+        self.bg_color = (230, 230, 230)
